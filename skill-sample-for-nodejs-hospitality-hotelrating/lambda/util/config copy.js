582a9c43beb47efa52c63455cedc94d5b3dd12af
BYRON SUN <byronsun@amazon.com>
2024-09-03 12:38:33 -0600

    update config file with comments

---
+++ skill-sample-for-nodejs-hospitality-hotelrating/lambda/util/config copy.js
@@ -0,0 +1,15 @@
+// Copyright 2024 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: LicenseRef-.amazon.com.-AmznSL-1.0
+// Licensed under the Amazon Software License  http://aws.amazon.com/asl/
+
+
+module.exports = Object.freeze({
+    //secrets manager region
+    secrets_mgr_region: 'us-west-2',
+
+    //secret name for 
+    secrets_mgr_secret_name: 'americas-ca-api-access-token',
+    secrets_mgr_asp_access_token_key: 'api-access-token',
+    asp_api_endpoint: 'https://api.amazonalexa.com/',
+    sns_topic_arn: 'arn:aws:sns:us-west-2:984667355978:StayBadRating'
+});
